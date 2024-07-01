4be73a1fca12de218a1187b6fa3722e102dcff19
Minty <mmeminty@gmail.com>
2024-07-01 22:15:24 +0200

    bugfix: fails on jpegs (e.g. https://www.smbc-comics.com/comic/2012-01-16); function parse_img() for image elements; feature: resuming via history file; feature: url, aftercomic saving; doubled wait; granular imports; main loop refactor

--- smbc-scraper.py
+++ smbc-scraper.py
@@ -15,80 +15,115 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import os
-from bs4 import BeautifulSoup
-import urllib
-import requests
-import shutil
-import random
-import time
+from os import makedirs
+from bs4 import BeautifulSoup, Tag
+from urllib.request import urlopen
+from urllib.parse import quote, urlsplit, urlunsplit
+from requests import get as requests_get
+from shutil import copyfileobj
+from random import randrange
+from time import sleep
 
-print("Sunday Morning Breakfast Cereal Scraper (smbc-comics.com) V0.1")
-print("***")
-print("If you enjoy Zach Weinersmith's work, please consider supporting him on Patreon. https://www.patreon.com/ZachWeinersmith?ty=h
-print("Alternatively, buy some SMBC merchandise: https://hivemill.com/collections/smbc")
-print("Pretty please with sugar on top. Patronage is what helps artists continue doing what they do best. Thank you!")
-print("***")
-print("Initializing...")
 
-# Hard-coding the initial URL
-first_url = 'https://www.smbc-comics.com/comic/2002-09-05'
-current_url = first_url
-next_url = first_url
-page_request = requests.get( first_url )
-parent_directory = os.getcwd()
-i = 1
+DIRECTORY: str = 'comics'
+EXTENSIONS: set = {'png', 'gif', 'jpg', 'jpeg',}
+def parse_img(element: Tag, stem: str):
+    url: str = element['src']
+    print("Downloading image from", url)
+    # SMBC uses different image formats
+    ext: str = url.split('.')[-1]
+    if ext not in EXTENSIONS:
+        print("Not a recognized image format!")
+        return
+
+    with urlopen(url) as response, \
+         open(f'{stem}.{ext}', 'wb') as f:
+            copyfileobj(response, f)
+
+
+# Random user agent to calm down any anti-bot measures
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    }
+def request(url):
+    return requests_get(url, headers=headers)
+
+
+print("""
+Sunday Morning Breakfast Cereal Scraper (smbc-comics.com) V0.2
+***
+If you enjoy Zach Weinersmith's work, please consider supporting him on Patreon. https://www.patreon.com/ZachWeinersmith?ty
+Alternatively, buy some SMBC merchandise: https://hivemill.com/collections/smbc
+Pretty please with sugar on top. Patronage is what helps artists continue doing what they do best. Thank you!
+***
+Initializing...
+""")
+
+
+i: int
+try:
+    with open('history.txt', 'r') as f:
+        i = int(f.readline())
+        current_url = f.readline().strip()
+except FileNotFoundError:
+    print("No history file found. Starting from the beginning.")
+    i = 1
+    current_url = 'https://www.smbc-comics.com/comic/2002-09-05'
+
 
 # While page exists, proceed
-while( page_request.status_code != 404 ):
+while True:
+    print("Processing comic number", i)
+    id: str = str(i).zfill(4)
+    stem: str = f'{DIRECTORY}/{id}'
+    makedirs(stem, exist_ok=True)
 
-	if next_url == 'https://www.smbc-comics.com/comic/2002-09-05':
-		current_page = requests.get( first_url )
-	else:
-		current_page = requests.get( next_url )
-	# Grab content of current page
-	html_content = current_page.text
-	soup = BeautifulSoup( html_content,'lxml' )
-	# Retrieving title text of the image. Use web developer tools to inspect the DOM.
-	title_text = soup.find( 'div', {'id':'cc-comicbody'} ).img['title'].encode('ascii','ignore').decode()
-	# Retrieving URL of the comic image
-	comic_image_url = soup.find( 'div',{'id':'cc-comicbody'} ).img['src']	
-	print("...")
-	print("Processing comic number ", i)
-	print("Image URL is: ", comic_image_url)
+    # Request current page
+    page = request(current_url)
+    if page.status_code != 200:
+        print(f"HTTP error encountered: {page.status_code}. Exiting...")
+        break
 
-	# Numbering the directories
-	comic_directory = str( i )
-	os.mkdir( comic_directory )
-	i = i + 1
-	os.chdir( comic_directory )
-	directory = os.getcwd()
-	# DEBUG LOG	print("Current directory is: ", directory )
-	
-	title_text_file = open( comic_directory + '.txt', 'w' )
-	title_text_file.write( title_text )
-	title_text_file.close()
-	# DEBUG LOG	print("Title text successfully written")
-	
-	# SMBC uses different image formats
-	if ( "png" in comic_image_url ):
-	# urllib.request.urlretrieve is considered deprecated https://docs.python.org/3/library/urllib.request.html#legacy-interface
-	# Using urlopen instead
-		with urllib.request.urlopen( comic_image_url ) as response, open( ( comic_directory + '.png'), 'wb') as out_file:
-		    shutil.copyfileobj(response, out_file)
-	elif ( "gif" in comic_image_url ):
-	# urllib.request.urlretrieve is considered deprecated https://docs.python.org/3/library/urllib.request.html#legacy-interface
-	# Using urlopen instead
-		with urllib.request.urlopen( comic_image_url ) as response, open( ( comic_directory + '.gif'), 'wb') as out_file:
-		    shutil.copyfileobj(response, out_file)
-	else:
-		print("Not a recognized image format!")
-	
-	next_url = soup.find('a',{'class':'cc-next'})['href']	
-	os.chdir( parent_directory )
-	
-	# Basic pseudo-random wait to avoid detection by the target server. Change the range of values to go faster
-	delay = random.randrange(3, 30)
-	print("Comic number successfully scraped.")
-	print("Waiting for ", delay, "seconds between requests to throw off anti-bot mitigation measures...")
-	time.sleep( delay )
+    with open(f'{stem}/url.txt', 'w') as f:
+        f.write(current_url)
+
+    # Grab content of current page
+    soup = BeautifulSoup(page.text, 'lxml')
+
+    # Retrieving title text
+    comic_image_element: Tag = soup.find(
+        'div', {'id': 'cc-comicbody'}).img
+    title: str = comic_image_element['title'].encode('ascii', 'ignore').decode()
+
+    with open(f'{stem}/title.txt', 'w') as f:
+        f.write(title)
+
+    # Retrieve main comic image
+    parse_img(comic_image_element, f'{stem}/comic')
+
+    # Check for aftercomic (red button) image
+    try:
+        aftercomic_image_element: Tag = soup.find(
+            'div', {'id': 'aftercomic'}).img
+        parse_img(aftercomic_image_element, f'{stem}/aftercomic')
+    except AttributeError:
+        print("No aftercomic image found!")
+
+    # Check for next comic URL
+    next_element: Tag = soup.find(
+        'a', {'class': 'cc-next'})
+    if not next_element:
+        print("No more comics found. Exiting...")
+        break
+
+    # Move on to next comic
+    i += 1
+    current_url = next_element['href']
+    with open('history.txt', 'w') as f:
+        f.write(f'{i}\n{current_url}')
+
+    # Basic pseudo-random wait to avoid detection by the target server. Change the range of values to go faster
+    print("Comic successfully scraped.")
+    print("Waiting for ", delay := randrange(6, 60),
+          "seconds between requests to throw off anti-bot mitigation measures...")
+    sleep(delay)
